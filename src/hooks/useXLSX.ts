d582792368affd42e62e08414be080228cfa6956
MSI\gn01792218 <gn01792218@hotmail.com>
2023-08-22 12:10:38 +0800

    [refactor] add excel file restriction、error handle、set input file name

--- src/hooks/useXLSX.ts
+++ src/hooks/useXLSX.ts
@@ -1,10 +1,9 @@
 
 import { read } from 'xlsx'
 export default function useXLSX(){
-    
-
     async function handleExcelFileInput(event:React.ChangeEvent<HTMLInputElement>){
         if(!event.currentTarget.files?.length) return 
+        if(!event.currentTarget.files[0].name.includes("xlsx")) return alert('請上傳excel使用的.xlsx檔案')
         const file = event.currentTarget.files[0]
         const data = await file.arrayBuffer()
         return read(data)
--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -6,26 +6,32 @@ function Home() {
     const { handleExcelFileInput } = useXLSX()
 
     const [source, setSourceFile] = useState<WorkBook>()
+    const [sourceFileName, setSourceFileName] = useState<string>()
+    const [compareFileName, setCompareFileName] = useState<string>()
     const [comparison, setComparisonFile] = useState<WorkBook>()
     async function handleSourceFile(e: React.ChangeEvent<HTMLInputElement>) {
         const file = await handleExcelFileInput(e)
+        if(!file) return
         setSourceFile(file)
+        setSourceFileName(e.target.files![0].name)
     }
     async function handleComparisonFile(e: React.ChangeEvent<HTMLInputElement>) {
         const file = await handleExcelFileInput(e)
+        if(!file)return
         setComparisonFile(file)
+        setCompareFileName(e.target.files![0].name)
     }
-    function getClassNumber(className:string){
-        if(className.includes('一')) return 1
-        else if(className.includes('二')) return 2
-        else if(className.includes('三')) return 3
-        else if(className.includes('四')) return 4
-        else if(className.includes('五')) return 5
-        else if(className.includes('六')) return 6
-        else if(className.includes('七')) return 7
-        else if(className.includes('八')) return 8
-        else if(className.includes('九')) return 9
-        else if(className.includes('十')) return 10
+    function getClassNumber(className: string) {
+        if (className.includes('一')) return 1
+        else if (className.includes('二')) return 2
+        else if (className.includes('三')) return 3
+        else if (className.includes('四')) return 4
+        else if (className.includes('五')) return 5
+        else if (className.includes('六')) return 6
+        else if (className.includes('七')) return 7
+        else if (className.includes('八')) return 8
+        else if (className.includes('九')) return 9
+        else if (className.includes('十')) return 10
     }
     function output() {
         if (!source || !comparison) return alert('請上傳好檔案')
@@ -34,29 +40,31 @@ function Home() {
         const sourceSheet = source.Sheets[sourceSheetsName]
         const comparisonSheet = comparison.Sheets[comparisonSheetsName]
         let row = 2
-        console.log(comparisonSheet)
-        while(sourceSheet[`B${row}`]){
-            const name =sourceSheet[`B${row}`].v
-            const set = sourceSheet[`F${row}`].v
-            let compareRow = 2
-            while(comparisonSheet[`E${compareRow}`]){
-                const compareName = comparisonSheet[`E${compareRow}`].v
-                if(name === compareName){
-                    const compareClassnumber = getClassNumber(comparisonSheet[`A${compareRow}`].v)
-                    const compareSetNumber = comparisonSheet[`B${compareRow}`].v
-                    utils.sheet_add_aoa(sourceSheet,[[compareSetNumber]],{origin:`H${row}`})
-                    utils.sheet_add_aoa(sourceSheet,[[compareClassnumber]],{origin:`G${row}`})
-                    compareRow = 2
-                    break
+        try{
+            while (sourceSheet[`B${row}`]) {
+                const name = sourceSheet[`B${row}`].v
+                const set = sourceSheet[`F${row}`].v
+                let compareRow = 2
+                while (comparisonSheet[`E${compareRow}`]) {
+                    const compareName = comparisonSheet[`E${compareRow}`].v
+                    if (name === compareName) {
+                        const compareClassnumber = getClassNumber(comparisonSheet[`A${compareRow}`].v)
+                        const compareSetNumber = comparisonSheet[`B${compareRow}`].v
+                        utils.sheet_add_aoa(sourceSheet, [[compareSetNumber]], { origin: `H${row}` })
+                        utils.sheet_add_aoa(sourceSheet, [[compareClassnumber]], { origin: `G${row}` })
+                        compareRow = 2
+                        break
+                    }
+                    utils.sheet_add_aoa(sourceSheet, [['轉學']], { origin: `G${row}` })
+                    compareRow++
                 }
-                utils.sheet_add_aoa(sourceSheet,[['轉學']],{origin:`G${row}`})
-                compareRow++
+                row++
             }
-            row++
+            const data = write(source, { bookType: 'xlsx', type: 'file' })
+            FileSaver.saveAs(data)
+        }catch(e){
+            alert(e+'可能是來源檔案或比對檔案有誤，致使程式無法做運算')
         }
-        
-        const data = write(source,{bookType:'xlsx',type:'file'})
-        FileSaver.saveAs(data)
     }
     return (
         <div className='text-white'>
@@ -65,11 +73,17 @@ function Home() {
                     使用須知 : 輸出檔案時，請記得檔案名稱後面要加上 <span className="text-red-300">.xlsx</span> 的副檔名唷~!
                 </p>
                 <div>
-                    <label className="cursor-pointer hover:text-slate-500" htmlFor="source"> + 來源檔案</label>
+                    <label className="flex cursor-pointer hover:text-slate-500" htmlFor="source">
+                        <p className="mr-2">+ 來源檔案</p>
+                        <p className="text-slate-500">{sourceFileName}</p>
+                    </label>
                     <input className="hidden" id='source' type="file" onChange={handleSourceFile} />
                 </div>
                 <div >
-                    <label className="cursor-pointer hover:text-slate-500" htmlFor="compare"> + 比對檔案</label>
+                    <label className="flex cursor-pointer hover:text-slate-500" htmlFor="compare">
+                        <p className="mr-2">+ 比對檔案</p>
+                        <p className="text-slate-500">{compareFileName}</p>
+                    </label>
                     <input className="hidden" id='compare' type="file" onChange={handleComparisonFile} />
                 </div>
             </header>
