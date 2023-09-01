3441cd39ab8ae6766c52f1cf3fcb8863a05ba34c
MSI\gn01792218 <gn01792218@hotmail.com>
2023-09-01 12:14:18 +0800

    [refactor] add valid excel file column and refactor output

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -7,31 +7,6 @@ function Home() {
     const [sourceFileName, setSourceFileName] = useState<string>()
     const [compareFileName, setCompareFileName] = useState<string>()
     const [comparison, setComparisonFile] = useState<WorkBook>()
-    async function handleSourceFile(e: React.ChangeEvent<HTMLInputElement>) {
-        const file = await handleExcelFileInput(e)
-        if (!file) return
-        setSourceFile(file)
-        setSourceFileName(e.target.files![0].name)
-    }
-    async function handleComparisonFile(e: React.ChangeEvent<HTMLInputElement>) {
-        const file = await handleExcelFileInput(e)
-        if (!file) return
-        setComparisonFile(file)
-        setCompareFileName(e.target.files![0].name)
-    }
-    function getClassNumber(className: string) {
-        const classStr = className.substring(2)
-        if (classStr.includes('一')) return 1
-        else if (classStr.includes('二')) return 2
-        else if (classStr.includes('三')) return 3
-        else if (classStr.includes('四')) return 4
-        else if (classStr.includes('五')) return 5
-        else if (classStr.includes('六')) return 6
-        else if (classStr.includes('七')) return 7
-        else if (classStr.includes('八')) return 8
-        else if (classStr.includes('九')) return 9
-        else if (classStr.includes('十')) return 10
-    }
     async function output() {
         if (!source || !comparison) return alert('請上傳好檔案')
         const sourceSheetsName = source.SheetNames[0]
@@ -41,11 +16,14 @@ function Home() {
         let row = 2
         try{
             while (sourceSheet[`A${row}`] || sourceSheet[`B${row}`]) {
-                const name = sourceSheet[`B${row}`].v
-                const studentNumber =sourceSheet[`A${row}`].v
                 let compareRow = 2
                 while (comparisonSheet[`E${compareRow}`]) {
-                    const compareName = comparisonSheet[`E${compareRow}`].v
+                    if(!sourceSheet[`A${row}`]) {
+                        utils.sheet_add_aoa(sourceSheet, [['查無資料']], { origin: `G${row}` })
+                        compareRow++
+                        break
+                    }
+                    const studentNumber =sourceSheet[`A${row}`].v
                     const compareStudentNumber = comparisonSheet[`C${compareRow}`].v
                     if (studentNumber === compareStudentNumber) {
                         const compareClassnumber = getClassNumber(comparisonSheet[`A${compareRow}`].v)
@@ -68,7 +46,46 @@ function Home() {
             致使程式無法比對，
             請確認上傳符合格式內容之excel檔案。`,)
         }
-        writeFileXLSX(source,'nurse_system.xls')  //下載檔案
+        writeFileXLSX(source!,'nurse_system.xls')  //下載檔案
+    }
+    async function handleSourceFile(e: React.ChangeEvent<HTMLInputElement>) {
+        const file = await handleExcelFileInput(e)
+        if (!validSourceFile(file!)) return alert('請上傳正確的SSHIS檔案格式:A欄title只能是學號，或學生學號')
+        setSourceFile(file)
+        setSourceFileName(e.target.files![0].name)
+    }
+    async function handleComparisonFile(e: React.ChangeEvent<HTMLInputElement>) {
+        const file = await handleExcelFileInput(e)
+        if (!validCompareFile(file!)) return alert('請上傳正確的學務系統檔案格式，確認C欄title為學號')
+        setComparisonFile(file)
+        setCompareFileName(e.target.files![0].name)
+    }
+    function getClassNumber(className: string) {
+        const classStr = className.substring(2)
+        if (classStr.includes('一')) return 1
+        else if (classStr.includes('二')) return 2
+        else if (classStr.includes('三')) return 3
+        else if (classStr.includes('四')) return 4
+        else if (classStr.includes('五')) return 5
+        else if (classStr.includes('六')) return 6
+        else if (classStr.includes('七')) return 7
+        else if (classStr.includes('八')) return 8
+        else if (classStr.includes('九')) return 9
+        else if (classStr.includes('十')) return 10
+    }
+    function validSourceFile(file:WorkBook){
+        if(!file) return false
+        const sheet = file.Sheets[file.SheetNames[0]] 
+        const colA = sheet["A1"].v
+        if( colA!== "學號" && colA!=="學生學號" ) return false
+        return true
+    }
+    function validCompareFile(file:WorkBook){
+        if(!file) return false
+        const sheet = file.Sheets[file.SheetNames[0]] 
+        const colC = sheet["C1"].v
+        if( colC!== "學號" && colC!=="學生學號" ) return false
+        return true
     }
     return (
         <div className='text-white text-left flex flex-col items-center p-5'>
