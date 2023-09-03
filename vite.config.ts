b5afa00b000421d27fc01e6d3ce5dc099d11c08a
MSI\gn01792218 <gn01792218@hotmail.com>
2023-09-04 01:13:31 +0800

    convert to PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import AutoImport from 'unplugin-auto-import/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 const {resolve} = require('path')
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    VitePWA({
+      
+    }),
     AutoImport({
       imports:['react', 'react-router-dom'],
       dts:'src/auto-imports.d.ts'
